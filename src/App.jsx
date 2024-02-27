d62da17a27d0f256018f2478d7f17cdd6c17a67e
unknown <randyleean092m@gmail.com>
2024-02-27 18:48:23 +1300

    feat: set up routing options for navbar.
    files: added images to be used for website.
    
    bug: importing @mui/material doesn't seem to work? Dependency installation issue? I have no clue.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Box from "@mui/material";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+
+export default function App() {
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
+  );
+}
---
+++ src/pages/ExerciseDetail.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ExerciseDetail() {
+  return <div>ExerciseDetail</div>;
+}
