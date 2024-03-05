c01fe754e45b02d54b3f26d1121cefe699e2e6c2
unknown <randyleean092m@gmail.com>
2024-03-05 23:36:35 +1300

    bugFix: environment variable name must have a prefix VITE_VARIABLENAME to exposed it as import.meta.env.VITE_VARIABLENAME to my client soruce code.

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': import.meta.env.EXERCISE_DB_RAPID_API_KEY,
+      'X-RapidAPI-Key': import.meta.env.VITE_EXERCISE_DB_RAPID_API_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
